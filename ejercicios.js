e73a9e676ff7df194bfedfb2258e7efa359937d1
Carlos Estrada <kachiiz@gmail.com>
2022-01-25 17:04:18 -0600

    repaso variables, ejercicio arreglos

---
+++ ejercicios.js
@@ -0,0 +1,21 @@
+// Crea una variable que almacene el resultado de la operación  23 al cubo
+
+// Crea una nueva cadena de texto que una las variables listadas utilizando una coma intermedia
+    const saludo = 'Hola'
+    const pregunta = 'cómo estás?'
+
+// Calcula el área de un círculo cuyo radio es de 15cm
+
+// Crea un arreglo que contenga 11 elementos de cualquier tipo de dato
+
+// Asigna a la variable llamada 'postreFavorito' el primer elemento del arreglo 'todosLosPostres' 
+let todosLosPostres = ['flan', 'pastel', 'jericalla', 'gelatina', 'carlota'];
+let postreFavorito
+
+// Cuantos 1s hay en el arreglo 'arregloDeUnos'
+
+// Del arreglo arregloUsuarios, sustituye a Julio por Luis 
+let arregloUsuarios = ['Diego', 'Julio', 'Ana', 'Paula'];
+
+// Guarda en la variable suma la suma de los tres números del arreglo numeros
+let numeros = [12, 4, 8];
---
+++ variables.js
@@ -0,0 +1,31 @@
+// HTML - Estructura
+// CSS - como se ve la página
+// Javascript - lógica (funcionalidad)
+
+// Variables
+// Declaración de una variable
+var nombreVariable = 88;
+// let & const
+let valorQueCambia = 123;
+valorQueCambia = 456;
+
+const valorPi = 3.14159; 
+
+// Asignación de valores ( = )
+nombreVariable = 12;
+
+// Tipo de datos
+// number: 4, 5, 3.14, -9, 0
+// string: 'Sarah' "Me gustan las galletas :)"
+// boolean: true / false
+// array: []
+// object:
+
+// Array = Arreglo
+let misLibros = [
+   "El sr de los anillos",
+   "HP y el algo que no me acuerdo", 
+   "alicia en wonderland",
+];
+
+misLibros[0];
