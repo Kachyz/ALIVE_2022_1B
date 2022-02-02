d9c58fb5609e71183180d27ce18ef6dd31eed018
Carlos Estrada <kachiiz@gmail.com>
2022-02-01 18:09:29 -0600

    Feb 01 - funciones

---
+++ Feb_01.js
@@ -0,0 +1,16 @@
+// Funciones
+// qué sabemos de las funciones ?
+
+// es como una receta de cocina
+
+function saludar(persona) { // parentesis reciben información
+    // las instrucciones a ejecutar de esta función
+    console.log("Hola, " + persona + ". Cómo estás?")
+}
+
+// Crear una función que reciba 2 números e imprima en pantalla
+// la multiplicación de los mismos
+function multiplica(numero1, numero2) {
+    let resultado = numero1 * numero2;
+    console.log("el resultado es: " + resultado)
+}
